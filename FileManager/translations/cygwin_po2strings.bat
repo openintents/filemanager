4f5235fd22642230e97f248229e8906e20f3f42c
Peli <peli0101@gmail.com>
2011-01-29 23:41:30 +0000

    (split)Translation: Delete obsolete files. All is translated centrally.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@3076 72b678ce-9140-0410-bee8-679b907dd61a

--- FileManager/translations/cygwin_po2strings.bat
+++
@@ -1,2 +0,0 @@
-set PATH=.;c:\cygwin\bin;%PATH%
-bash androidxml2po.bash -i
--- FileManager/translations/cygwin_strings2po.bat
+++
@@ -1,5 +0,0 @@
-set PATH=.;c:\cygwin\bin;%PATH%
-bash androidxml2po.bash -e
-mkdir translations_filemanager
-copy filemanager* translations_filemanager
-tar -cvvzf translations_filemanager.tgz translations_filemanager

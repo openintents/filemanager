21f7864cc9f07c503278a95b4a659293992e0105
George Venios <veniosg@gmail.com>
2012-08-05 22:21:38 +0300

    Added proper file picker resolving on PickFileListFragment.

--- FileManager/src/org/openintents/filemanager/lists/PickFileListFragment.java
+++ FileManager/src/org/openintents/filemanager/lists/PickFileListFragment.java
@@ -1,5 +1,7 @@
 package org.openintents.filemanager.lists;
 
+import java.io.File;
+
 import org.openintents.filemanager.FileManagerProvider;
 import org.openintents.filemanager.PreferenceActivity;
 import org.openintents.filemanager.R;
@@ -13,6 +15,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class PickFileListFragment extends SimpleFileListFragment{
 	
@@ -29,18 +32,43 @@ public class PickFileListFragment extends SimpleFileListFragment{
 	}
 	
 	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		
+		if(getArguments().getBoolean(FileManagerIntents.EXTRA_DIRECTORIES_ONLY)){
+			Button pickButton = (Button) view.findViewById(R.id.button);
+			pickButton.setVisibility(View.VISIBLE);
+			pickButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					pickFileOrFolder(new File(getPath()), false);
+				}
+			});
+		}
+	}
+	
+	@Override
 	protected void openFile(FileHolder fileholder) {
 		// GET_CONTENT or PICK_FILE request. If it was PICK_DIRECTORY, we wouldn't have shown files, and therefore we would have never reached this call.
-		Intent intent = new Intent();
-		PreferenceActivity.setDefaultPickFilePath(getActivity(), fileholder.getFile().getParent() != null ? 
-				fileholder.getFile().getParent() : "/");
 		
-		if(getArguments().getBoolean(FileManagerIntents.EXTRA_IS_GET_CONTENT_INITIATED, false))
-			intent.setData(Uri.parse(FileManagerProvider.FILE_PROVIDER_PREFIX + fileholder.getFile()));
+		pickFileOrFolder(fileholder.getFile(), getArguments().getBoolean(FileManagerIntents.EXTRA_IS_GET_CONTENT_INITIATED, false));
+
+	}
+	
+	/**
+	 * Act upon picking. 
+	 * @param selection A {@link File} representing the user's selection.
+	 * @param getContentInitiated Whether the fragment was called through a GET_CONTENT intent on the IntentFilterActivity. We have to know this so that result is correctly formatted.
+	 */
+	private void pickFileOrFolder(File selection, boolean getContentInitiated){
+		Intent intent = new Intent();
+		PreferenceActivity.setDefaultPickFilePath(getActivity(), selection.getParent() != null ?  selection.getParent() : "/");
+		
+		if(getContentInitiated)
+			intent.setData(Uri.parse(FileManagerProvider.FILE_PROVIDER_PREFIX + selection));
 		else
-			intent.setData(Uri.fromFile(fileholder.getFile()));
+			intent.setData(Uri.fromFile(selection));
 		getActivity().setResult(Activity.RESULT_OK, intent);
 		getActivity().finish();
-		return;
 	}
 }

380415ad5ed67dbd46b5fb5502cc2febdc5e5da3
vaibhav3011 <vaibhav.devpura@gmail.com>
2013-01-12 01:06:42 +0530

    Extra MOVE action

--- FileManager/src/org/openintents/filemanager/IntentFilterActivity.java
+++ FileManager/src/org/openintents/filemanager/IntentFilterActivity.java
@@ -10,6 +10,7 @@ import org.openintents.filemanager.util.UIUtils;
 import org.openintents.intents.FileManagerIntents;
 
 import android.content.Intent;
+import android.drm.DrmStore.Action;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
@@ -23,9 +24,9 @@ public class IntentFilterActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstance) {
 		UIUtils.setThemeFor(this);
-		
+
 		super.onCreate(savedInstance);
-		
+
 		Intent intent = getIntent();
 
 		// Initialize arguments
@@ -49,15 +50,16 @@ public class IntentFilterActivity extends FragmentActivity {
 
 		// Add a path if a path has been specified in this activity's call.
 		File data = FileUtils.getFile(getIntent().getData());
-		if (data != null) {			
-			File dir = FileUtils.getPathWithoutFilename(data);		
+		if (data != null) {
+			File dir = FileUtils.getPathWithoutFilename(data);
 			if (dir != null) {
 				extras.putString(FileManagerIntents.EXTRA_DIR_PATH,
 						data.getAbsolutePath());
 			}
-			if (dir != data){
+			if (dir != data) {
 				// data is a file
-				extras.putString(FileManagerIntents.EXTRA_FILENAME, data.getName());
+				extras.putString(FileManagerIntents.EXTRA_FILENAME,
+						data.getName());
 			}
 		}
 
@@ -74,7 +76,7 @@ public class IntentFilterActivity extends FragmentActivity {
 
 	private void chooseListType(Intent intent, Bundle extras) {
 		// Multiselect
-		if (!intent.getAction().isEmpty()) {
+		if (FileManagerIntents.ACTION_MULTI_SELECT.equals(intent.getAction())) {
 			String tag = "MultiSelectListFragment";
 			mFragment = (MultiselectListFragment) getSupportFragmentManager()
 					.findFragmentByTag(tag);
@@ -113,12 +115,12 @@ public class IntentFilterActivity extends FragmentActivity {
 				// Pass extras through to the list fragment. This helps
 				// centralize the path resolving, etc.
 				extras.putBoolean(
-						FileManagerIntents.EXTRA_IS_GET_CONTENT_INITIATED,
-						intent.getAction().equals(Intent.ACTION_GET_CONTENT));
-				extras.putBoolean(
 						FileManagerIntents.EXTRA_DIRECTORIES_ONLY,
 						intent.getAction().equals(
 								FileManagerIntents.ACTION_PICK_DIRECTORY));
+				extras.putBoolean(
+						FileManagerIntents.EXTRA_IS_GET_CONTENT_INITIATED,
+						intent.getAction().equals(Intent.ACTION_GET_CONTENT));
 
 				mFragment.setArguments(extras);
 				getSupportFragmentManager()
--- FileManager/src/org/openintents/filemanager/lists/PickFileListFragment.java
+++ FileManager/src/org/openintents/filemanager/lists/PickFileListFragment.java
@@ -22,94 +22,114 @@ import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
-public class PickFileListFragment extends SimpleFileListFragment{
+public class PickFileListFragment extends SimpleFileListFragment {
 	private PickBar mPickBar;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setLongClickMenus(R.menu.context_pick, R.menu.multiselect);
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.filelist_pick, null);
 	}
-	
+
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
-		ViewFlipper modeSelector = (ViewFlipper) view.findViewById(R.id.modeSelector);
-		
+		ViewFlipper modeSelector = (ViewFlipper) view
+				.findViewById(R.id.modeSelector);
+
 		// Folder init
-		if(getArguments().getBoolean(FileManagerIntents.EXTRA_DIRECTORIES_ONLY)){
+		if (getArguments()
+				.getBoolean(FileManagerIntents.EXTRA_DIRECTORIES_ONLY)) {
 			modeSelector.setDisplayedChild(0);
-			
+
 			Button button = (Button) view.findViewById(R.id.button);
-			button.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					pickFileOrFolder(new File(getPath()), false);
-				}
-			});
-			if(getArguments().containsKey(FileManagerIntents.EXTRA_BUTTON_TEXT)){
-				button.setText(getArguments().getString(FileManagerIntents.EXTRA_BUTTON_TEXT));
+			if (getArguments()
+					.containsKey(FileManagerIntents.EXTRA_BUTTON_TEXT)) {
+				button.setText(getArguments().getString(
+						FileManagerIntents.EXTRA_BUTTON_TEXT));
+
+				button.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						pickFileOrFolder(new File(getPath()), false);
+					}
+				});
 			}
 		}
 		// Files init
 		else {
 			modeSelector.setDisplayedChild(1);
-			
+
 			mPickBar = (PickBar) view.findViewById(R.id.pickBar);
-			mPickBar.setButtonText(getArguments().getString(FileManagerIntents.EXTRA_BUTTON_TEXT));
-			 
+			mPickBar.setButtonText(getArguments().getString(
+					FileManagerIntents.EXTRA_BUTTON_TEXT));
+
 			mPickBar.setText(getFilename());
-			// TODO make file visible, i.e. scroll to item 
-			
+			// TODO make file visible, i.e. scroll to item
+
 			mPickBar.setOnPickRequestedListener(new OnPickRequestedListener() {
 				@Override
 				public void pickRequested(String filename) {
-					if(filename.trim().length() == 0) {
-						Toast.makeText(getActivity(), R.string.choose_filename, Toast.LENGTH_SHORT).show();
+					if (filename.trim().length() == 0) {
+						Toast.makeText(getActivity(), R.string.choose_filename,
+								Toast.LENGTH_SHORT).show();
 						return;
 					}
 
 					// Pick
-					pickFileOrFolder(new File(getPath() + (getPath().endsWith("/") ? "" : "/") + filename), 
-							getArguments().getBoolean(FileManagerIntents.EXTRA_IS_GET_CONTENT_INITIATED, false));
+					pickFileOrFolder(
+							new File(getPath()
+									+ (getPath().endsWith("/") ? "" : "/")
+									+ filename),
+							getArguments()
+									.getBoolean(
+											FileManagerIntents.EXTRA_IS_GET_CONTENT_INITIATED,
+											false));
 				}
 			});
-			
-		}			
+
+		}
 	}
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		FileHolder item = (FileHolder) mAdapter.getItem(position);
-		
-		if(item.getFile().isFile())
+
+		if (item.getFile().isFile())
 			mPickBar.setText(item.getName());
 		else
 			super.onListItemClick(l, v, position, id);
 	}
-	
+
 	/**
-	 * Act upon picking. 
-	 * @param selection A {@link File} representing the user's selection.
-	 * @param getContentInitiated Whether the fragment was called through a GET_CONTENT intent on the IntentFilterActivity. We have to know this so that result is correctly formatted.
+	 * Act upon picking.
+	 * 
+	 * @param selection
+	 *            A {@link File} representing the user's selection.
+	 * @param getContentInitiated
+	 *            Whether the fragment was called through a GET_CONTENT intent
+	 *            on the IntentFilterActivity. We have to know this so that
+	 *            result is correctly formatted.
 	 */
-	private void pickFileOrFolder(File selection, boolean getContentInitiated){
+	private void pickFileOrFolder(File selection, boolean getContentInitiated) {
 		Intent intent = new Intent();
 		intent.putExtras(getArguments());
-		
-		PreferenceActivity.setDefaultPickFilePath(getActivity(), selection.getParent() != null ?  selection.getParent() : "/");
-		
-		if(getContentInitiated)
-			intent.setData(Uri.parse(FileManagerProvider.FILE_PROVIDER_PREFIX + selection));
+
+		PreferenceActivity.setDefaultPickFilePath(getActivity(),
+				selection.getParent() != null ? selection.getParent() : "/");
+
+		if (getContentInitiated)
+			intent.setData(Uri.parse(FileManagerProvider.FILE_PROVIDER_PREFIX
+					+ selection));
 		else
-			intent.setData(Uri.fromFile(selection));		
+			intent.setData(Uri.fromFile(selection));
 		getActivity().setResult(Activity.RESULT_OK, intent);
 		getActivity().finish();
 	}
